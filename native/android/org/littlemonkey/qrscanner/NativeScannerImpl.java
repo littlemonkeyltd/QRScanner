72b3820adc44a2ed306a7099f02a4eb1b49a5ad2
nick <nick@nick-All-Series>
2014-04-29 16:42:27 +1200

    Working and returning Error values - CAM-9 done

--- native/android/org/littlemonkey/qrscanner/NativeScannerImpl.java
+++ native/android/org/littlemonkey/qrscanner/NativeScannerImpl.java
@@ -22,19 +22,22 @@ public class NativeScannerImpl {
         final android.app.Activity ctx = com.codename1.impl.android.AndroidNativeUtil.getActivity();
         Intent intent = new Intent(ctx, ZBarScannerActivity.class);
         if (qrCode) {
-        intent.putExtra(ZBarConstants.SCAN_MODES, new int[]{Symbol.QRCODE});
+            intent.putExtra(ZBarConstants.SCAN_MODES, new int[]{Symbol.QRCODE});
         } else {
             intent.putExtra(ZBarConstants.SCAN_MODES, new int[]{Symbol.EAN13});
         }
         com.codename1.impl.android.AndroidNativeUtil.startActivityForResult(intent, new IntentResultListener() {
             public void onActivityResult(int requestCode, int resultCode, Intent data) {
-                Log.p("SCANNER " + requestCode + " " + resultCode + " " + data.getStringExtra(ZBarConstants.SCAN_RESULT));
                 if (resultCode == Activity.RESULT_OK) {
                     QRScanner.getCallback().scanCompleted(data.getStringExtra(ZBarConstants.SCAN_RESULT), "QRCODE", data.getStringExtra(ZBarConstants.SCAN_RESULT).getBytes());
-                } else if (resultCode == Activity.RESULT_CANCELED && data != null) {
-                    String error = data.getStringExtra(ZBarConstants.ERROR_INFO);
-                    if (!TextUtils.isEmpty(error)) {
-                        QRScanner.getCallback().scanError(100, error);
+                } else if (resultCode == Activity.RESULT_CANCELED) {
+                    if (data != null) {
+                        String error = data.getStringExtra(ZBarConstants.ERROR_INFO);
+                        if (!TextUtils.isEmpty(error)) {
+                            QRScanner.getCallback().scanError(100, error);
+                        } else {
+                            QRScanner.getCallback().scanCanceled();
+                        }
                     } else {
                         QRScanner.getCallback().scanCanceled();
                     }
